8b7b7fab23fe40c7a98ea5c8aa8e02919e95717a
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2024-04-16 13:40:55 -0700

    initial commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { Search } from '@mui/icons-material'
+import Link from 'next/link'
+import SearchIcon from '@mui/icons-material/Search';
+import React, { useEffect, useState } from 'react'
+
+const Navbar = () => {
+    const [search, setSearch] = useState<string>("");
+    const [dropdownMenu, setDropdownMenu] = useState<boolean>(false);
+
+    const [isScolled, setIsScrolled] = useState<boolean>(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 10) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+        }
+
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <div className={`navbar ${isScolled && "bg-black-1"}`}>
+            <Link href="/">
+                <img src="/assets/logo.png" alt='logo' className='logo' />
+            </Link>
+
+            <div className="nav-links">
+                <Link href="/" className='nav-link'>Home</Link>
+                <Link href="/my-list" className='nav-link'>My List</Link>
+            </div>
+
+            <div className='nav-right'>
+                <div className="search">
+                    <input
+                        placeholder='Search movie...'
+                        className='input-search'
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <Search className="icon" />
+                </div>
+
+                <img
+                    src="/assets/profile_icon.jpg"
+                    className='profile'
+                    alt="profile"
+                    onClick={() => setDropdownMenu(!dropdownMenu)} />
+
+                {dropdownMenu && (
+                    <div className="dropdown-menu">
+                        <Link href="/" >Home</Link>
+                        <Link href="/my-list">My List</Link>
+                        <a href="">Log Out</a>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
